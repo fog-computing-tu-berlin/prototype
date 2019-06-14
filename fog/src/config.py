a4829abda2066044ac468d4602b118f92ed8fa3f
dihmandrake <jasper.bernhardt@live.de>
2019-06-15 00:06:44 +0200

    Fog | Implement Config DI

---
+++ fog/src/config.py
@@ -0,0 +1,19 @@
+from asyncio import AbstractEventLoop
+
+
+class Config:
+
+    def __init__(self, async_loop: AbstractEventLoop) -> None:
+        super().__init__()
+        self.__async_loop = async_loop
+
+    def get_control_message_tick_rate(self) -> int:
+        # 0 For only publish on request
+        # Otherwise value in Hz
+        return 4
+
+    def get_async_loop(self) -> AbstractEventLoop:
+        return self.__async_loop
+
+    def get_control_message_ticker_update_timeout(self) -> int:
+        return 3600
