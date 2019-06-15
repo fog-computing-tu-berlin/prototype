af53b4e93b3e8a0c4c8bd922577eacd9f7a7256a
dihmandrake <jasper.bernhardt@live.de>
2019-06-15 12:14:39 +0200

    Cloud | Adjust Sensor table for Numeric

--- cloud/database/V2__Sensor_table.sql
+++ cloud/database/V2__Sensor_table.sql
@@ -1,16 +1,17 @@
 CREATE TABLE public.sensor
 (
-    -- TODO correct data values
     id SERIAL NOT NULL,
-    sensor_id varchar(128) NOT NULL,
-    temperature REAL NOT NULL,
-    humidity REAL NOT NULL,
-    uv REAL NOT NULL,
+    temperature NUMERIC(5, 3) NOT NULL,
+    temperature_sensor_id varchar(128) NOT NULL,
+    humidity NUMERIC(5, 3) NOT NULL,
+    humidity_sensor_id varchar(128) NOT NULL,
+    uv NUMERIC(5, 3) NOT NULL,
+    uv_sensor_id varchar(128) NOT NULL,
     started_at timestamp NOT NULL,
     stopped_at timestamp NOT NULL,
     PRIMARY KEY (id)
 )
-TABLESPACE pg_default;
+    TABLESPACE pg_default;
 
 ALTER TABLE public.sensor
     OWNER to postgres;
