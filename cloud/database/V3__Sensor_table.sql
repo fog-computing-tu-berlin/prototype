d24353a1f6a280ff34fa5ffff33b95885037dcbd
dihmandrake <jasper.bernhardt@live.de>
2019-06-15 12:38:47 +0200

    Cloud | Edge ID Table

--- cloud/database/V3__Sensor_table.sql
+++ cloud/database/V3__Sensor_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE public.sensor
 (
-    id SERIAL NOT NULL,
+    id bigserial PRIMARY KEY NOT NULL,
+    edge_id bigint REFERENCES edge_id(id) NOT NULL,
     temperature NUMERIC(5, 3) NOT NULL,
     temperature_sensor_id varchar(128) NOT NULL,
     humidity NUMERIC(5, 3) NOT NULL,
@@ -8,15 +9,13 @@ CREATE TABLE public.sensor
     uv NUMERIC(5, 3) NOT NULL,
     uv_sensor_id varchar(128) NOT NULL,
     started_at timestamp NOT NULL,
-    stopped_at timestamp NOT NULL,
-    PRIMARY KEY (id)
-)
-    TABLESPACE pg_default;
+    stopped_at timestamp NOT NULL
+);
 
 ALTER TABLE public.sensor
     OWNER to postgres;
 
-GRANT SELECT, INSERT(sensor_id, temperature, humidity, uv, started_at, stopped_at)
+GRANT SELECT, INSERT(edge_id, temperature, temperature_sensor_id, humidity, humidity_sensor_id, uv, uv_sensor_id, started_at, stopped_at)
     ON sensor TO anon;
 
 GRANT USAGE ON SEQUENCE sensor_id_seq TO anon;
