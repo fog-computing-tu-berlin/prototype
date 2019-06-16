11d2813bef091704219dabefb0aec6477b2ee156
Elias Grünewald <gruenewald@campus.tu-berlin.de>
2019-06-16 17:40:02 +0200

    Built full working frontend and extended test script for fog

--- fog/dev/tryZMQ.py
+++ fog/dev/tryZMQ.py
@@ -1,7 +1,8 @@
 import zmq
 from random import randint
+import time
 
-num_runs = 100
+num_runs = 1000
 url_client = "tcp://localhost:5555"
 
 context = zmq.Context()
@@ -9,16 +10,17 @@ print("Connecting to server...")
 socket = context.socket(zmq.REQ)
 socket.connect(url_client)
 
-timestamp = 1560348074409
+timestamp = randint(1560698424, 1660698424)
 sensor = 1
 while num_runs > 0:
-    timestamp += randint(10, 5000)
+    timestamp += randint(300, 30000)
     humidity = randint(1000, 9500)
     temperature = randint(1500, 3500) 
     uv = randint(1, 5)
     socket.send_string("{},0HF1{},1EKx{},2xkb{},{},2762".format(sensor, humidity, temperature, uv, timestamp))
     # socket.send_string("myplant,0HF14737,1EKx2862,2xkb1,1560348074409,2762")
-    print("Message sent")
+    print(timestamp)
     response = socket.recv_string()
     print(response)
     num_runs = num_runs - 1
+    time.sleep(0.300)
